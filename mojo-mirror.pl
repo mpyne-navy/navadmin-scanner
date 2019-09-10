c2f9d9f540febc031be80d42472cae8dd5f8ec20
Michael Pyne <michael.pyne@gmail.com>
2019-09-09 22:33:44 -0400

    Initial commit of NAVADMIN lister.
    Assumes you've already downloaded a relevant .aspx from the upstream
    website and prints out the URLs to all the NAVADMIN links present.

---
+++ mojo-mirror.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use v5.18;
+
+use Mojo::DOM;
+use Mojo::Collection;
+use Mojo::File;
+
+my $BASE_URL = 'https://www.public.navy.mil/bupers-npc/reference/messages/NAVADMINS/Pages/default.aspx';
+
+# Reads a binary blob of HTML and pulls out list of .txt hyperlinks
+sub read_navadmin_listing
+{
+    my $dom = Mojo::DOM->new(shift);
+    my $links_ref = $dom->find('a')
+        ->grep(sub { ($_->attr("href") // "") =~ qr/\.txt$/})
+        ->map(attr => 'href')
+        ->to_array;
+
+    return @{$links_ref};
+}
+
+my $file = Mojo::File->new($ARGV[0]) or die "No such file";
+my @links = read_navadmin_listing($file->slurp);
+
+say $_ foreach @links;
