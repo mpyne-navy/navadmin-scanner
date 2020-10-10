0b5be0c31e9db52f07180b45754ba27185f39a19
Michael Pyne <michael.pyne@gmail.com>
2020-10-10 19:22:32 -0400

    Add a simple Navbar.

--- navadmin-viewer.pl
+++ navadmin-viewer.pl
@@ -208,6 +208,46 @@ This server has NAVADMINs on file for the following years:
     <link rel="stylesheet" href="/css/bulma.css">
   </head>
   <body>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item">NAVADMIN Scanner/Viewer</a>
+
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarMenu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarMenu" class="navbar-menu">
+      <div class="navbar-start">
+        <a href="/" class="navbar-item">Home</a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            More
+          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              About
+            </a>
+            <a class="navbar-item">
+              Contact
+            </a>
+            <hr class="navbar-divider">
+            <a class="navbar-item">
+              Report an issue
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+      </div>
+    </div>
+  </nav>
+
   <section class="section">
     <div class="container">
       <%= content %>
