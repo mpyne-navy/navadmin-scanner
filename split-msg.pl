2e51f9b2c54f31ddf7923e37d4a74c972595f827
Michael Pyne <michael.pyne@gmail.com>
2023-01-25 22:34:53 -0500

    Add initial header field parser.

--- split-msg.pl
+++ split-msg.pl
@@ -67,11 +67,82 @@ sub split_up_navadmin($text)
     return ($head, $body);
 }
 
+sub decode_msg_head($head)
+{
+    # for now, just split every non-narrative line by first word
+    my %fields = (
+        REF => [ ],
+    );
+
+    my @lines = split(/\r?\n/, $head);
+    my $in_field = 0;
+    my $partial_field = '';
+
+    my $set_field = sub($field, $payload) {
+        if ($field eq 'REF') {
+            my ($id, $info) = split(/\//, $payload, 2);
+            push @{$fields{REF}}, { id => $id, text => $info };
+        } else {
+            $fields{$field} = $payload;
+        }
+    };
+
+    for my $line (@lines) {
+        if ($in_field) {
+            # if we're reading a slash-separated field, append lines to field
+            # until it ends in //.
+            $partial_field .= $line;
+            if ($line =~ m,// *$,) {
+                # end of field found
+
+                my ($field, $payload) = split(/ *\/ */, $partial_field, 2);
+                $set_field->($field, $payload);
+                $in_field = 0;
+                $partial_field = '';
+            }
+
+            next;
+        }
+
+        # we weren't already in a field, check for common line types
+        next if $line =~ /^$/;
+        next if $line =~ /^ *PASS TO OFFICE/;
+        next if $line =~ /^ *CLASSIFICATION:/;
+        next if $line =~ /^ *(BT|UNCLAS|ROUTINE|R |O |P )/;
+
+        # seems to be a real field, see if the first separator is a space or a
+        # slash.  If a slash, look for a double-slash as the field terminator.
+        if ($line =~ /^[A-Z]+ /) {
+            # space
+            my ($field, $payload) = split(' ', $line, 2);
+            $set_field->($field, $payload);
+        } elsif ($line =~ /^[A-Z]+ *\//) {
+#           say "line has a slash [$line]";
+            # slash
+            if ($line !~ m,// *$,) {
+                # this line only has part of the field. Don't parse until we
+                # have the whole field
+#               say "\tline is partial [$line]";
+                $in_field = 1;
+                $partial_field = $line;
+                next;
+            }
+
+            # Got the whole field here, read it
+            my ($field, $payload) = split(/ *\/ */, $line, 2);
+            $set_field->($field, $payload);
+        }
+    }
+
+    return \%fields;
+}
+
 sub read_navadmin($path)
 {
     my $content = $path->slurp;
     my ($head, $body) = split_up_navadmin($content);
-    say encode_json({ head => $head, body => $body });
+    my $fields = decode_msg_head($head);
+    say encode_json({ head => $head, fields => $fields, body => $body });
 }
 
 my @files;
